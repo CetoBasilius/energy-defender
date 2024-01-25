9cec5bee144aa053920045cb95cb22ecc288317b
Basilio German <basiliogerman@gmail.com>
2024-01-25 00:24:06 -0700

    Added Tower placement logic with snap and color indication

--- Assets/Scripts/Ui/TowerDrag.cs
+++ Assets/Scripts/Ui/TowerDrag.cs
@@ -7,7 +7,10 @@ public class TowerDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public GameObject testTowerPrefab;
     public GameObject towers;
     public Tilemap tilemap;
+    public Color placeableColor = Color.green;
+    public Color nonPlaceableColor = Color.red;
     private GameObject draggedTower;
+    private Vector3Int lastCell;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -17,34 +20,76 @@ public class TowerDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnDrag(PointerEventData eventData)
     {
-        // Move the dragged tower with the mouse
         if (draggedTower != null)
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = draggedTower.transform.position.z;
             draggedTower.transform.position = mousePosition;
+
+            UpdateTileColor();
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        TileBase tile = GetGridTileUnderMouse();
-        if (tile != null)
+        TileBase tilebase = GetGridTileUnderMouse();
+        if (tilebase != null)
         {
-            Debug.Log("Dropped tower on " + tile.name + " at " + tilemap.WorldToCell(draggedTower.transform.position));
+            bool isPlaceable = tilebase.name == "grass";
+            if (!isPlaceable)
+            {
+                Destroy(draggedTower);
+            }
+            else
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int cellPosition = tilemap.WorldToCell(mousePosition);
+                Vector3 snapPosition = tilemap.GetCellCenterWorld(cellPosition);
+                snapPosition.z = draggedTower.transform.position.z;
+                draggedTower.transform.position = snapPosition;
+
+                Debug.Log("Dropped tower on " + tilebase.name + " at " + cellPosition);
+
+                // TODO: set tile to tower so no other tower can be placed there or improve placement logic
+            }
         }
         else
         {
             Destroy(draggedTower);
         }
+
+        ClearLastCell();
     }
 
     private TileBase GetGridTileUnderMouse()
     {
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int coordinate = tilemap.WorldToCell(mouseWorldPos);
-        TileBase tile = tilemap.GetTile(coordinate);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int cellPosition = tilemap.WorldToCell(mousePosition);
+        TileBase tilebase = tilemap.GetTile(cellPosition);
 
-        return tile;
+        return tilebase;
+    }
+
+    private void ClearLastCell()
+    {
+        tilemap.SetTileFlags(lastCell, TileFlags.None);
+        tilemap.SetColor(lastCell, Color.white);
+    }
+
+    private void UpdateTileColor()
+    {
+        ClearLastCell();
+        TileBase tilebase = GetGridTileUnderMouse();
+        if (tilebase != null)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPosition = tilemap.WorldToCell(mousePosition);
+
+            bool isPlaceable = tilebase.name == "grass";
+
+            tilemap.SetTileFlags(cellPosition, TileFlags.None);
+            tilemap.SetColor(cellPosition, isPlaceable ? placeableColor : nonPlaceableColor);
+            lastCell = cellPosition;
+        }
     }
 }
